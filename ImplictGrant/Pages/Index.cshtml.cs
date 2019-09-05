e21ef7f3b08f9d24bb42ee5496858801f4e5b49a
Sri Gunnala <srikanth.gunnala@gmail.com>
2019-09-05 21:01:56 +1000

    add project

---
+++ ImplictGrant/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ImplictGrant.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            var request = HttpContext.Request;
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //https://pitangui.amazon.com/spa/skill/account-linking-status.html?vendorId=M2AAAAAAAAAAAA#state=xyz&access_token=2YotnFZFEjr1zCsicMWpAA&token_type=Bearer
+
+            var url = request.Query["redirect_uri"] + "#state=" + request.Query["state"] + "&access_token=2YotnFZFEjr1zCsicMWpAA&token_type=Bearer";
+            return Redirect(url);
+        }
+    }
+}
